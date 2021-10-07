c9040f9ec5a8f3576a8fb0d1fd0c6acfe8a8f9b1
JoshuaJL2021 <JoshuaJLopez@yahoo.com>
2021-10-07 16:12:28 -0400

    test

---
+++ Models/Customer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Models
+{
+    public class Customer
+    {
+        private string _name;
+        public string CustomerName
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        
+
+        private string _address;
+        public string Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+
+        private string _contact;
+        public string Contact
+        {
+            get { return _contact; }
+            set { _contact = value; }
+        }
+
+        private List<Orders> customerOrders = new List<Orders>();
+        public List<Orders> MyOrders
+            {
+                set { customerOrders = value; }
+                get { return customerOrders; }
+            }
+    }
+}
