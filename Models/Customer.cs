6802ce93e96584de3b786af5ddd3a172c1676d03
JoshuaJL2021 <JoshuaJLopez@yahoo.com>
2021-10-14 12:06:08 -0400

    Modified version

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -5,16 +5,26 @@ namespace Models
 {
     public class Customer
     {
+        //Created to print out name of corresponding customer on each menu when succesfully signed in
+        public static string username;
+        public static string password;
+
+
         //fields
 
          public string _name;
          public string _address;
          public string _contact;
-public List<Orders> customerOrders = new List<Orders>();
+        public List<Orders> customerOrders = new List<Orders>();
+        //constructors
+        public Customer(string name, string address, string contact){
+
+        }
+        public Customer(){}
 
         //in the JSON file the information would be entered in the way the constructors are for each property
         //property that uses the field name
-                public string CustomerName
+        public string CustomerName
         {
             get { return _name; }
             set { _name = value; }
@@ -41,15 +51,20 @@ public List<Orders> customerOrders = new List<Orders>();
                 set { customerOrders = value; }
                 get { return customerOrders; }
             }
-       public Customer(string name, string address, string contact){
-
+            public int _age;
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
         }
-        public Customer(){}
+    
 
-//in order to simply print out a customers information completely in a writeline
+        //in order to simply print out a customers information completely in a writeline
         public override string ToString()
         {
             return $"Name: {_name}\nAddress: {_address}\nContact: {_contact}";
         }
+
+    
     }
 }
